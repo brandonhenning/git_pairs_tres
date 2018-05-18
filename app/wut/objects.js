96c92a147af4f12fdd4c08b3a3bba9443e9f7619
Brandon Henning <brandonhenning@Brandons-MacBook-Pro-3.local>
2018-05-18 11:49:13 -0600

    added object array to objects.js and added to wut directory

---
+++ app/wut/objects.js
@@ -0,0 +1,6 @@
+const drinkers = [
+    {name: 'Captain Taylor', age: 21}, 
+    {name: 'First Mate Spencer', age: 21},
+    {name: 'Navigator Brandon', age: 91},
+    {name: 'Kitchen-Hand Kaitlin', age: 18}
+]
